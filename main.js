5063ce32a34e7d6daa91eb3b7e328e3be9403429
bahromjonAbduvosit <79294650+bahromjonAbduvosit@users.noreply.github.com>
2022-06-02 00:26:54 +0500

    finish todo app

--- main.js
+++ main.js
@@ -9,14 +9,25 @@ const app = Vue.createApp({
                 {id:1, name:"Bahrom", email:"bahromkarimov321@gmail.com", job:"frontend devoloper"}
             ],
             errorHandler:'',
-            error:false
+            error:false,
+            handleUserId:null
         }
     },
 
 
     methods:{
         addUser(){
-            if(this.name && this.email && this.job){
+            if(this.handleUserId!=null) {
+                let findUserId = this.users.find((user)=>user.id==this.handleUserId)
+                findUserId.name=this.name,
+                findUserId.email=this.email,
+                findUserId.job=this.job,
+                this.handleUserId=null,
+                this.name=''
+                this.email=""
+                this.job=''
+            }
+            else if(this.name && this.email && this.job){
                 console.log(this.name, this.email, this.job);
                 let newUser={
                     id:this.users.length+1,
@@ -28,6 +39,7 @@ const app = Vue.createApp({
                 this.name=''
                 this.email=""
                 this.job=''
+                this.error=false
             }
             else{
                 // alert('Input is empty')
@@ -43,7 +55,8 @@ const app = Vue.createApp({
             let findUserId = this.users.find((user)=>user.id==id)
             this.name=findUserId.name,
             this.email=findUserId.email,
-            this.job=findUserId.job
+            this.job=findUserId.job,
+            this.handleUserId= id
         }
     }
 
